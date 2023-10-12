b904276df6dc059e786090a20b08abf8d54902bb
Al3x-K <ola.kail1901@gmail.com>
2023-10-12 10:44:21 +0100

    added an enemy and enemy movement script

--- Assets/scripts/AdvancedPlayerMovement.cs
+++ Assets/scripts/AdvancedPlayerMovement.cs
@@ -1,73 +1,125 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
 
 public class AdvancedPlayerMovement : MonoBehaviour
 {
     public float speed = 10f;
-    public float jumpHeight = 7f;
+    public float jumpHeight = 7f; 
     public float dashSpeed = 20f;
-    public float crouchHeight = 5f;
-
-    public LayerMask whatisGround;
-    public Transform groundCheckPoint;
+    public float crouchHeight = 0.5f;
+    public LayerMask whatIsGround; 
+    public Transform groundCheckPoint; 
     public float groundCheckRadius = 0.2f;
 
-    public AudioClip jumpSouind;
-    public AudioClip dashSound;
-    public AudioClip footStepSound;
+    public AudioClip jumpSound; 
+    public AudioClip dashSound; 
+    public AudioClip footstepSound; 
 
-    private Rigidbody2D body;
-
-    private Animator anim;
-
-    private AudioSource audioSource;
-
-    private bool grounded;
-    private bool canDoubleJump = false;
-    private bool isDashing = false;
+    private Rigidbody2D body; 
+    private Animator anim; 
+    private AudioSource audioSource; 
+    public bool grounded; 
+    public bool canDoubleJump = false;
+    private bool isDashing = false; 
     private bool isCrouching = false;
-    private bool facingRight = true;
+    private bool facingRight = true; 
+
+
+    
     // Start is called before the first frame update
-    private void Awake()
+    void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        grounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatisGround);
-
+        grounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+        
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-        body.velocity = new Vector2(horizontalInput*speed,body.velocity.y);
-        anim.SetBool("walk",horizontalInput != 0);
+        body.velocity = new Vector2(horizontalInput*speed, body.velocity.y);
+        anim.SetBool("walk", horizontalInput !=0);
 
-        if((horizontalInput>0 && !facingRight)||(horizontalInput<0 && facingRight))
+        if(horizontalInput != 0 && grounded)
+        {
+            PlaySound(footstepSound);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)&& grounded)
+        {
+             Jump();
+            canDoubleJump = true; 
+             
+        }
+        else if(Input.GetKeyDown(KeyCode.Space)&&canDoubleJump)
+        {
+            Jump();
+            canDoubleJump = false;
+        }
+
+        if((horizontalInput>0&& !facingRight)||(horizontalInput<0&&facingRight))
         {
             Flip();
         }
-        if(Input.GetKey(KeyCode.Space)&&grounded)
+
+        if(Input.GetKey(KeyCode.LeftShift) && !isDashing)
         {
-            Jump();
+            StartCoroutine(Dash());
+        }
+        
+        if (Input.GetKeyDown(KeyCode.DownArrow)&& grounded)
+        {
+            if(!isCrouching)
+            {
+                transform.localScale = new Vector3(transform.localScale.x, crouchHeight, transform.localScale.z);
+                isCrouching = true;
+            }
+             else if (isCrouching)
+            {
+                transform.localScale = new Vector3(transform.localScale.x,1f,transform.localScale.z);
+                isCrouching = false; 
+            }
         }
 
+        
     }
-
-     private void Flip()
+    private void Flip()
     {
-        Vector3 currentScale = gameObject.transform.localScale;
+        Vector3 currentScale = gameObject.transform.localScale; 
         currentScale.x *= -1;
-        gameObject.transform.localScale = currentScale;
-        facingRight = !facingRight;
+        gameObject.transform.localScale = currentScale; 
+        facingRight = !facingRight; 
     }
-
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x , jumpHeight);
-        grounded = false; 
-        anim.SetTrigger("Jump");
+        body.velocity = new Vector2(body.velocity.x, jumpHeight);
+        anim.SetTrigger("jump");
+        grounded = false;
+        PlaySound(jumpSound);
     }
 
+    IEnumerator Dash()
+    {
+        PlaySound(dashSound);
+        float originalSpeed = speed; 
+        speed = dashSpeed; 
+        isDashing = true; 
+        yield return new WaitForSeconds(0.2f);
+        speed = originalSpeed; 
+        isDashing = false; 
+    }
+    private void PlaySound(AudioClip clip)
+    {
+        audioSource.clip = clip; 
+        audioSource.Play();
+    }
+
+    
 }
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 10f;
-    public float jumpHeight = 7f;
+    public float speed = 10f; 
+    public float jumpHeight = 7f; 
     private Rigidbody2D body;
-    private Animator anim;
-    private bool grounded;
-    private bool facingRight = true;
-    // Start is called before the first frame update
-    void Awake()
+    private Animator anim; 
+    private bool grounded; 
+    private bool facingRight = true; 
+
+    private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
@@ -21,40 +20,37 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        body.velocity = new Vector2(horizontalInput*speed, body.velocity.y);
-        anim.SetBool("Walk", horizontalInput != 0);
+        float horizontalInput =  Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        anim.SetBool("walk",horizontalInput!=0);
         
-        if((horizontalInput>0 && !facingRight)||(horizontalInput<0 && facingRight))
+        if((horizontalInput>0 && !facingRight)|| (horizontalInput<0&& facingRight))
         {
             Flip();
         }
-        if(Input.GetKey(KeyCode.Space)&&grounded)
+
+        if(Input.GetKey(KeyCode.Space)&& grounded)
         {
-            Jump();
+        Jump();
         }
     }
 
-    private void Jump()
-    {
-        body.velocity = new Vector2(body.velocity.x , jumpHeight);
-        grounded = false; 
-        anim.SetTrigger("Jump");
+    private void Jump(){
+        body.velocity = new Vector2(body.velocity.x, jumpHeight);
+        anim.SetTrigger("jump");
+        grounded = false;
     }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
+    private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.CompareTag("Ground"))
-        {
-            grounded = true;
-        }
+        grounded = true;
     }
-
     private void Flip()
     {
         Vector3 currentScale = gameObject.transform.localScale;
         currentScale.x *= -1;
         gameObject.transform.localScale = currentScale;
-        facingRight = !facingRight;
+        facingRight = !facingRight; 
     }
+
+
 }
